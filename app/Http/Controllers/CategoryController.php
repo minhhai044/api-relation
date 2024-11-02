372662155218a4e70f9eb6e7b85fb1feb08de188
minhhai044 <tmhai2004@gmail.com>
2024-11-02 22:05:56 +0700

    category+tag

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -2,7 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoteCategoryRequest;
+use App\Http\Requests\UpdateCategoryRequest;
+use App\Models\Category;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Log;
 
 class CategoryController extends Controller
 {
@@ -11,15 +17,30 @@ class CategoryController extends Controller
      */
     public function index()
     {
-        //
+        $data = Category::query()->latest('id')->paginate(10);
+        return response()->json([
+            'data' => $data
+        ], Response::HTTP_OK);
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(StoteCategoryRequest $request)
     {
-        //
+        try {
+            $data = $request->validated();
+            $category = Category::query()->create($data);
+            return response()->json([
+                'messenge' => 'Thêm mới thành công !!!',
+                'data' => $category
+            ], Response::HTTP_CREATED);
+        } catch (\Throwable $th) {
+            Log::debug(__CLASS__ . '@' . __FUNCTION__, [$th->getMessage()]);
+            return response()->json([
+                'messenge' => 'Thêm mới không thành công !!!'
+            ], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
     }
 
     /**
@@ -27,15 +48,44 @@ class CategoryController extends Controller
      */
     public function show(string $id)
     {
-        //
+        try {
+            $data = Category::query()->findOrFail($id);
+            return response()->json([
+                'data' => $data
+            ]);
+        } catch (\Throwable $th) {
+            if ($th instanceof ModelNotFoundException) {
+                return response()->json([
+                    'messenge' => 'Không tồn tại'
+                ], Response::HTTP_NOT_FOUND);
+            }
+            Log::debug(__CLASS__ . '@' . __FUNCTION__, [$th->getMessage()]);
+
+            return response()->json([
+                'messenge' => 'Lỗi hệ thống'
+            ], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(UpdateCategoryRequest $request, string $id)
     {
-        //
+        try {
+            $data = $request->validated();
+            $category = Category::query()->findOrFail($id);
+            $category->update($data);
+            return response()->json([
+                'messenge' => 'Update thành công !!',
+                'data' =>  $category
+            ], Response::HTTP_OK);
+        } catch (\Throwable $th) {
+            Log::debug(__CLASS__ . '@' . __FUNCTION__, [$th->getMessage()]);
+            return response()->json([
+                'messenge' => 'Update không thành công !!!'
+            ], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
     }
 
     /**
@@ -43,6 +93,17 @@ class CategoryController extends Controller
      */
     public function destroy(string $id)
     {
-        //
+        try {
+            $category = Category::query()->findOrFail($id);
+            $category->delete();
+            return response()->json([
+                'messenge' => 'Delete thành công !!!'
+            ], Response::HTTP_OK);
+        } catch (\Throwable $th) {
+            Log::debug(__CLASS__ . '@' . __FUNCTION__, [$th->getMessage()]);
+            return response()->json([
+                'messenge' => 'Delete không thành công !!!'
+            ], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
     }
 }
--- app/Http/Controllers/TagController.php
+++ app/Http/Controllers/TagController.php
@@ -2,7 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoteTagRequest;
+use App\Http\Requests\UpdateTagRequest;
+use App\Models\Tag;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Log;
 
 class TagController extends Controller
 {
@@ -11,15 +17,30 @@ class TagController extends Controller
      */
     public function index()
     {
-        //
+        $data = Tag::query()->latest('id')->paginate(10);
+        return response()->json([
+            'data' => $data
+        ], Response::HTTP_OK);
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(StoteTagRequest $request)
     {
-        //
+        try {
+            $data = $request->validated();
+            $category = Tag::query()->create($data);
+            return response()->json([
+                'messenge' => 'Thêm mới thành công !!!',
+                'data' => $category
+            ], Response::HTTP_CREATED);
+        } catch (\Throwable $th) {
+            Log::debug(__CLASS__ . '@' . __FUNCTION__, [$th->getMessage()]);
+            return response()->json([
+                'messenge' => 'Thêm mới không thành công !!!'
+            ], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
     }
 
     /**
@@ -27,15 +48,44 @@ class TagController extends Controller
      */
     public function show(string $id)
     {
-        //
+        try {
+            $data = Tag::query()->findOrFail($id);
+            return response()->json([
+                'data' => $data
+            ]);
+        } catch (\Throwable $th) {
+            if ($th instanceof ModelNotFoundException) {
+                return response()->json([
+                    'messenge' => 'Không tồn tại'
+                ], Response::HTTP_NOT_FOUND);
+            }
+            Log::debug(__CLASS__ . '@' . __FUNCTION__, [$th->getMessage()]);
+
+            return response()->json([
+                'messenge' => 'Lỗi hệ thống'
+            ], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(UpdateTagRequest $request, string $id)
     {
-        //
+        try {
+            $data = $request->validated();
+            $category = Tag::query()->findOrFail($id);
+            $category->update($data);
+            return response()->json([
+                'messenge' => 'Update thành công !!',
+                'data' =>  $category
+            ], Response::HTTP_OK);
+        } catch (\Throwable $th) {
+            Log::debug(__CLASS__ . '@' . __FUNCTION__, [$th->getMessage()]);
+            return response()->json([
+                'messenge' => 'Update không thành công !!!'
+            ], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
     }
 
     /**
@@ -43,6 +93,17 @@ class TagController extends Controller
      */
     public function destroy(string $id)
     {
-        //
+        try {
+            $category = Tag::query()->findOrFail($id);
+            $category->delete();
+            return response()->json([
+                'messenge' => 'Delete thành công !!!'
+            ], Response::HTTP_OK);
+        } catch (\Throwable $th) {
+            Log::debug(__CLASS__ . '@' . __FUNCTION__, [$th->getMessage()]);
+            return response()->json([
+                'messenge' => 'Delete không thành công !!!'
+            ], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
     }
 }
